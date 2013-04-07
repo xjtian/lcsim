247c81374b5e0f94c51805df72a2b848aecc42c9
Jacky Tian <xjtian94@gmail.com>
2013-04-07 01:23:47 -0400

    Digital source base class and an arbitrary digital source class, with all tests.

--- components/sources.py
+++ components/sources.py
@@ -3,25 +3,47 @@ __author__ = 'Jacky'
 from components import base
 
 
-class DigitalZero(base.ComponentBase):
+class DigitalSourceBase(base.ComponentBase):
+    """
+    Base class for all digital source components
+    """
+    def __init__(self, name, output):
+        """
+        Initialize a new digital source component.
+
+        @param output: A list of bits (0 or 1) that this component will always evaluate to.
+        """
+        if any([x != 0 and x != 1 for x in output]):
+            raise ValueError('Bits can only be 0 or 1! Invalid value encountered.')
+
+        super(DigitalSourceBase, self).__init__(name, 0, len(output))
+        # Keep a private version just in case output_bits is changed somehow.
+        self.__exp_output = output
+        self.output_bits = output
+
+    def evaluate(self):
+        self.output_bits = self.__exp_output
+
+
+class DigitalZero(DigitalSourceBase):
     """
     Digital source component that always outputs 0.
     """
     def __init__(self):
-        super(DigitalZero, self).__init__('D0', 0, 1)
-        self.output_bits[0] = 0
-
-    def evaluate(self):
-        self.output_bits[0] = 0
+        super(DigitalZero, self).__init__('D0', [0])
 
 
-class DigitalOne(base.ComponentBase):
+class DigitalOne(DigitalSourceBase):
     """
     Digital source component that always outputs 1.
     """
     def __init__(self):
-        super(DigitalOne, self).__init__('D1', 0, 1)
-        self.output_bits[0] = 1
+        super(DigitalOne, self).__init__('D1', [1])
 
-    def evaluate(self):
-        self.output_bits[0] = 1
+
+class DigitalArbitrary(DigitalSourceBase):
+    """
+    Any arbitrary digital source that has no inputs and always outputs a set number and arrangement of bits.
+    """
+    def __init__(self, output):
+        super(DigitalArbitrary, self).__init__('DArb', output)
--- components/test/test_sources.py
+++ components/test/test_sources.py
@@ -4,6 +4,32 @@ import unittest
 from components import sources
 
 
+class TestDigitalSourceBase(unittest.TestCase):
+    """
+    Test for DigitalSourceBase class.
+    """
+    def test_constructor(self):
+        gate = sources.DigitalSourceBase('Test', [0, 1, 1])
+
+        self.assertEqual('Test', gate.name)
+        self.assertEqual([0, 1, 1], gate.output_bits)
+        self.assertEqual(0, len(gate._input_bits))
+
+        # Make sure exception is raised if non-bit value is passed
+        try:
+            sources.DigitalSourceBase('Test', [1, 0, 1, 2])
+            self.fail('Digital source allowed non-bit value')
+        except ValueError:
+            self.assert_(True)
+
+    def test_evaluate(self):
+        gate = sources.DigitalSourceBase('Test', [0, 1, 1, 0, 1, 1])
+        # Arbitrarily modify output_bits to make sure it goes back to what it should be
+        gate.output_bits = [1, 2, 3]
+        gate.evaluate()
+        self.assertEqual([0, 1, 1, 0, 1, 1], gate.output_bits)
+
+
 class TestDigitalZero(unittest.TestCase):
     """
     Test for DigitalZero class.
@@ -18,6 +44,7 @@ class TestDigitalZero(unittest.TestCase):
         self.assertEqual([0], self.gate.output_bits)
 
     def test_evaluate(self):
+        self.gate.output_bits = [2]
         self.gate.evaluate()
         self.assertEqual([0], self.gate.output_bits)
 
@@ -36,5 +63,26 @@ class TestDigitalOne(unittest.TestCase):
         self.assertEqual([1], self.gate.output_bits)
 
     def test_evaluate(self):
+        self.gate.output_bits = [2]
         self.gate.evaluate()
         self.assertEqual([1], self.gate.output_bits)
+
+
+class TestDigitalArbitrary(unittest.TestCase):
+    """
+    Test for DigitalArbitrary class.
+    """
+    def setUp(self):
+        self.output = [0, 1, 1, 0]
+        self.gate = sources.DigitalArbitrary(self.output)
+
+    def test_constructor(self):
+        self.assertEqual('DArb', self.gate.name)
+        self.assertEqual(0, len(self.gate._input_bits))
+        self.assertEqual(len(self.output), len(self.gate.output_bits))
+        self.assertEqual(self.output, self.gate.output_bits)
+
+    def test_evaluate(self):
+        self.gate.output_bits = [2]
+        self.gate.evaluate()
+        self.assertEqual(self.output, self.gate.output_bits)
