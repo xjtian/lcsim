53aa15863acd42c44a8dda94e31a9fa08cd8d993
Jacky Tian <xjtian94@gmail.com>
2013-05-01 18:47:24 -0400

    Circuit rewrite, doctests only.

--- circuits/circuit.py
+++ circuits/circuit.py
@@ -1,20 +1,42 @@
 __author__ = 'Jacky'
 
 
+class InvalidCircuitException(Exception):
+    """
+    An InvalidCircuitException is thrown by a circuit when not all input and
+    output spaces have been filled on evaluation.
+    """
+    def __init__(self, message=''):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
 class Circuit(object):
     """
     Base class for all circuits. Abstractly, a circuit is just a graph
     consisting of circuit elements as nodes and the wires between them as
     edges.
 
-    In this representation, only the first and last "levels" of the circuit
-    are explicitly tracked. The input spaces of all first-level components
-    represent the input space of the circuit, and the output spaces of all
-    last-level components represent the output space of the circuit.
+    Each available slot in the input space of the circuit maps directly to
+    an available input slot for some gate, and each available output slot
+    maps to an available output slot for some gate.
 
-    In this way, the circuit can also be thought of as a function n->m bits.
+    Example Usage:
+        >>> from components import gates, sources
+        >>> a = gates.ANDGate()
+        >>> b = gates.ORGate()
+        >>> s = sources.DigitalArbitrary([0, 0, 1, 0])
+        >>> a.add_input(s, {0: 0, 1: 1})
+        >>> b.add_input(s, {2: 0, 3: 1})
+        >>> c = Circuit('test', 0, 2)
+        >>> c.add_output_component(a, {0: 0})
+        >>> c.add_output_component(b, {1: 0})
+        >>> c.evaluate()
+        [0, 1]
     """
-    def __init__(self, name, inputs, outputs):
+    def __init__(self, name, input_size, output_size):
         """
         Initialize a base circuit, basically a network of connected circuit
         components.
@@ -23,122 +45,94 @@ class Circuit(object):
             name:
                 The name for this circuit, can be any arbitrary string.
 
-            inputs:
-                If known, a list of circuit components (inherit from
-                ComponentBase) that form the first "level" of the circuit.
+            input_size:
+                The number of bits in the input space of the circuit.
 
-            outputs:
-                If known, a list of circuit components (inherit from
-                ComponentBase) that for the last "level" of the circuit.
-                These are the gates that form the output space of the circuit.
-
-        Example usage:
-            >>> from components import gates
-            >>> a = gates.ANDGate()
-            >>> b = gates.ORGate()
-            >>> c = gates.ANDGate()
-            >>> c.add_input(a, {0: 0})
-            >>> c.add_input(b, {0: 1})
-            >>> circuit = Circuit('example', [a, b], [c])
-            >>> circuit.input_size
-            4
-            >>> circuit.output_size
-            1
+            output_size:
+                The number of bits in the output space of the circuit.
         """
         self.name = name
-        self.inputs = inputs
-        self.outputs = outputs
 
-        self.input_size = 0
-        self.output_size = 0
+        # Each index represents an input space, and contains a tuple
+        # (Component, int) with the Component and its input bit number that
+        # corresponds to this input space.
+        self._inputs = [None] * input_size
 
-        for gate in self.inputs:
-            self.input_size += len(gate._input_bits)
+        # Each index represents an output space, and contains a tuple
+        # (Component, int) with the Component and its output bit number that
+        #  corresponds to this output space.
+        self._outputs = [None] * output_size
 
-        for gate in self.outputs:
-            self.output_size += len(gate.output_bits)
-
-    def append_input(self, component):
+    def add_input_component(self, component, mapping):
         """
-        Append a component to the input space of the circuit.
+        Set an input component for the circuit. Specify a mapping keyed by
+        the circuit input space to the component input bit.
 
         Parameters:
             component:
-                The circuit component (inherit from ComponentBase) to append
-                 to the input space of the circuit.
+                The component (inherit from ComponentBase) to add as an
+                input.
+            mapping:
+                int: int dictionary keyed by intended input space of this
+                circuit to the input bit number of the component.
 
-        Example usage:
-            >>> from components import gates
-            >>> c = Circuit('example', [], [])
-            >>> c.append_input(gates.ANDGate())
-            >>> c.input_size
-            2
+        Raises:
+            ValueError if the input space of the circuit is already taken
+            or any mapping values are out of range.
         """
-        self.inputs.append(component)
-        self.input_size += len(component._input_bits)
+        self.__add_component_helper(component, mapping, True)
 
-    def prepend_input(self, component):
+    def add_output_component(self, component, mapping):
         """
-        Prepend a component to the input space of the circuit.
+        Set an output component for the circuit. Specify a mapping keyed by
+        the circuit output space to the component output bit.
 
         Parameters:
             component:
-                The circuit component (inherit from ComponentBase) to
-                prepend to the input space of the circuit.
+                The component (inherit from ComponentBase) to add as an
+                output.
+            mapping:
+                int: int dictionary keyed by intended output space of this
+                circuit to the output bit number of the component.
 
-        Example usage:
-            >>> from components import gates
-            >>> c = Circuit('example', [], [])
-            >>> c.prepend_input(gates.ANDGate())
-            >>> c.input_size
-            2
+        Raises:
+            ValueError if the output space of the circuit is already taken
+            or any mapping values are out of range.
         """
-        self.inputs.insert(0, component)
-        self.input_size += len(component._input_bits)
+        self.__add_component_helper(component, mapping, False)
 
-    def append_output(self, component):
-        """
-        Append a component to the output space of the circuit.
+    def __add_component_helper(self, component, mapping, yes_input):
+        for k, v in mapping.iteritems():
+            if k < 0:
+                raise ValueError('Invalid input space index %d.' % k)
+            if v < 0:
+                raise ValueError('Invalid input component index %d' % v)
 
-        Parameters:
-            component:
-                The circuit component (inherit from ComponentBase) to append
-                 to the output space of the circuit.
+            if yes_input:
+                if self._inputs[k] is not None:
+                    raise ValueError('Circuit input space already taken.')
+                if v >= len(component._input_bits):
+                    raise ValueError('Invalid component input index %d' % v)
+            else:
+                if self._outputs[k] is not None:
+                    raise ValueError('Circuit output space already taken.')
+                if v >= len(component.output_bits):
+                    raise ValueError('Invalid component output index %d' % v)
 
-        Example usage:
-            >>> from components import gates
-            >>> c = Circuit('example', [], [])
-            >>> c.append_output(gates.ANDGate())
-            >>> c.output_size
-            1
-        """
-        self.outputs.append(component)
-        self.output_size += len(component.output_bits)
-
-    def prepend_output(self, component):
-        """
-        Prepend a component to the output space of the circuit.
-
-        Parameters:
-            component:
-                The circuit component (inherit from ComponentBase) to
-                prepend to the output space of the circuit.
-
-        Example usage:
-            >>> from components import gates
-            >>> c = Circuit('example', [], [])
-            >>> c.prepend_output(gates.ANDGate())
-            >>> c.output_size
-            1
-        """
-        self.outputs.insert(0, component)
-        self.output_size += len(component.output_bits)
+        for k, v in mapping.iteritems():
+            if yes_input:
+                self._inputs[k] = (component, v)
+            else:
+                self._outputs[k] = (component, v)
 
     def evaluate(self):
         """
-        Evaluate the result of the circuit. This method will raise an
-        exception if any components in the circuit are not fully connected (
-        i.e. have less input bits than minimally necessary).
+        Evaluate the result of the circuit. If any components in the circuit
+        are not fully connected (i.e. have less input connections than
+        minimally necessary) then a MissingInputException will bubble up.
+
+        If there are missing components in the input or output space this
+        method will raise an InvalidCircuitException.
 
         If all gates are fully connected, then the result of this method
         call will be that all the gates in the circuit are evaluated.
@@ -149,30 +143,23 @@ class Circuit(object):
             int list of evaluated output of circuit
 
         Raises:
-            MissingInputException from evaluate() calls to gates if any gate
-             is not connected.
-
-        Example usage:
-            >>> from components import gates, sources
-            >>> s = sources.DigitalArbitrary([0, 1, 1, 1])
-            >>> a = gates.ANDGate()
-            >>> b = gates.ORGate()
-            >>> a.add_input(s, {0: 0, 1: 1})
-            >>> b.add_input(s, {2: 0, 3: 1})
-            >>> circuit = Circuit('example', [s], [a, b])
-            >>> circuit.evaluate()
-            [0, 1]
+            InvalidCircuitException if there are any unspecified input or
+            output spaces.
         """
-        i = 0
-        output = [0] * self.output_size
+        if None in self._inputs:
+            raise InvalidCircuitException('Circuit inputs not fully '
+                                          'specified')
+        if None in self._outputs:
+            raise InvalidCircuitException('Circuit outputs not fully '
+                                          'specified')
 
-        for gate in self.outputs:
-            gate.evaluate()
-            for bit in gate.output_bits:
-                output[i] = bit
-                i += 1
+        result = [-1] * len(self._outputs)
+        for i, tup in enumerate(self._outputs):
+            com, j = tup
+            com.evaluate()
+            result[i] = com.output_bits[j]
 
-        return output
+        return result
 
 
 def connect_circuits(out_circuit, in_circuit, mapping):
@@ -187,8 +174,8 @@ def connect_circuits(out_circuit, in_circuit, mapping):
         in_circuit:
             Circuit (inherits from Circuit) to wire connections to.
         mapping:
-            dictionary of int: int keyed by output bit number of output
-             circuit to input bit number of target input circuit.
+            int: int dictionary keyed by space of output circuit to space of
+            input circuit.
 
     Raises:
         ValueError if the mapping is invalid in some way (connections
@@ -197,52 +184,37 @@ def connect_circuits(out_circuit, in_circuit, mapping):
 
     Example usage:
         >>> from components import gates, sources
-        >>> s = sources.DigitalArbitrary([0, 1])
-        >>> a = gates.ORGate()
-        >>> b = gates.NOTGate()
+        >>> a = gates.ANDGate()
+        >>> b = gates.ORGate()
+        >>> s = sources.DigitalArbitrary([1, 1, 0, 0])
+        >>> c = gates.XORGate()
+        >>> c1 = Circuit('left', 0, 2)
+        >>> c2 = Circuit('right', 2, 1)
         >>> a.add_input(s, {0: 0, 1: 1})
-        >>> c1 = Circuit('example1', [s], [a])
-        >>> c2 = Circuit('example2', [b], [b])
-        >>> connect_circuits(c1, c2, {0: 0})
+        >>> b.add_input(s, {2: 0, 3: 1})
+        >>> c1.add_output_component(a, {0: 0})
+        >>> c1.add_output_component(b, {1: 0})
+        >>> c2.add_input_component(c, {0: 0, 1: 1})
+        >>> c2.add_output_component(c, {0: 0})
+        >>> connect_circuits(c1, c2, {0: 0, 1: 1})
         >>> c2.evaluate()
-        [0]
+        [1]
     """
-    if len(mapping.keys()) > out_circuit.output_size or len(mapping.values()) >\
-            in_circuit.input_size:
-        raise ValueError('Connections must be one-to-one. Too many pairs'
-                         ' in mapping')
-
     for i in mapping.keys():
-        if i >= out_circuit.output_size:
+        if i >= len(out_circuit._outputs) or i < 0:
             raise ValueError('Invalid output bit number %d: not '
                              'addressable.' % i)
 
     for i in mapping.values():
-        if i >= in_circuit.input_size:
+        if i >= len(in_circuit._inputs) or i < 0:
             raise ValueError('Invalid input bit number %d: not '
                              'addressable.' % i)
 
-    output_cache = [None] * out_circuit.output_size
-    input_cache = [None] * in_circuit.input_size
+    for k, v in mapping.iteritems():
+        (com_out, i) = out_circuit._outputs[k]
+        (com_in, j) = in_circuit._inputs[v]
 
-    i = 0
-    for gate in out_circuit.outputs:
-        for j in xrange(0, len(gate.output_bits)):
-            output_cache[i+j] = (gate, i)
-        i += len(gate.output_bits)
-
-    i = 0
-    for gate in in_circuit.inputs:
-        for j in xrange(0, len(gate._input_bits)):
-            input_cache[i+j] = (gate, i)
-        i += len(gate._input_bits)
-
-    # Let the components handle the rest of the validation process
-    for k, v in mapping.items():
-        input_gate, i = input_cache[v]
-        output_gate, j = output_cache[k]
-
-        input_gate.add_input(output_gate, {k-j: v-i})
+        com_in.add_input(com_out, {i: j})
 
 
 def stack_circuits(name, c1, c2):
@@ -258,7 +230,6 @@ def stack_circuits(name, c1, c2):
         c1:
             Top circuit to stack. The input and output bit positions of this
             circuit will remain unchanged in the resulting stacked circuit.
-
         c2:
             Bottom circuit to stack. The input and output bit positions of
             this circuit will be shifted by the input and output size of c1,
@@ -269,19 +240,27 @@ def stack_circuits(name, c1, c2):
 
     Example usage:
         >>> from components import gates, sources
-        >>> s1 = sources.DigitalArbitrary([1, 1])
-        >>> s2 = sources.DigitalArbitrary([0, 0])
         >>> a = gates.ANDGate()
         >>> b = gates.ORGate()
-        >>> a.add_input(s1, {0: 0, 1: 1})
-        >>> b.add_input(s2, {0: 0, 1: 1})
-        >>> c1 = Circuit('example1', [s1], [a])
-        >>> c2 = Circuit('example2', [s2], [b])
-        >>> c3 = stack_circuits('example3', c1, c2)
+        >>> s = sources.DigitalArbitrary([1, 1, 0, 0])
+        >>> a.add_input(s, {0: 0, 1: 1})
+        >>> b.add_input(s, {2: 0, 3: 1})
+        >>> c1 = Circuit('top', 0, 1)
+        >>> c2 = Circuit('bottom', 0, 1)
+        >>> c1.add_output_component(a, {0: 0})
+        >>> c2.add_output_component(b, {0: 0})
+        >>> c3 = stack_circuits('name', c1, c2)
         >>> c3.evaluate()
         [1, 0]
     """
-    return Circuit(name, c1.inputs + c2.inputs, c1.outputs + c2.outputs)
+    new_inputs = c1._inputs + c2._inputs
+    new_outputs = c1._outputs + c2._outputs
+
+    result = Circuit(name, len(new_inputs), len(new_outputs))
+    result._inputs = new_inputs
+    result._outputs = new_outputs
+
+    return result
 
 
 def merge_circuits(name, c1, c2):
@@ -310,16 +289,25 @@ def merge_circuits(name, c1, c2):
 
     Example usage:
         >>> from components import gates, sources
-        >>> s1 = sources.DigitalArbitrary([0, 1, 1, 1])
-        >>> a = gates.ANDGate()
-        >>> b = gates.ORGate()
-        >>> c = gates.XORGate()
-        >>> a.add_input(s1, {0: 0, 1: 1})
-        >>> b.add_input(s1, {2: 0, 3: 1})
-        >>> left_circuit = Circuit('example', [s1], [a,b])
-        >>> right_circuit = Circuit('example2', [c], [c])
-        >>> connect_circuits(left_circuit, right_circuit, {0: 0, 1: 1})
-        >>> merge_circuits('example3', left_circuit, right_circuit).evaluate()
+        >>> a = gates.NOTGate()
+        >>> b = gates.XORGate()
+        >>> s = sources.DigitalArbitrary([1, 1, 0])
+        >>> c = gates.ORGate()
+        >>> c1 = Circuit('left', 0, 2)
+        >>> c2 = Circuit('right', 2, 1)
+        >>> a.add_input(s, {0: 0})
+        >>> b.add_input(s, {1: 0, 2: 1})
+        >>> c1.add_output_component(a, {0: 0})
+        >>> c1.add_output_component(b, {1: 0})
+        >>> c2.add_input_component(c, {0: 0, 1: 1})
+        >>> c2.add_output_component(c, {0: 0})
+        >>> connect_circuits(c1, c2, {0: 0, 1: 1})
+        >>> c3 = merge_circuits('merged', c1, c2)
+        >>> c3.evaluate()
         [1]
     """
-    return Circuit(name, c1.inputs, c2.outputs)
+    result = Circuit(name, len(c1._inputs), len(c2._outputs))
+    result._inputs = c1._inputs
+    result._outputs = c2._outputs
+
+    return result
