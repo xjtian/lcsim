f5a338b7e5290dd7ad1cd85359fb564a261a3abc
Jacky Tian <xjtian94@gmail.com>
2013-05-03 16:07:45 -0400

    Unit tests for bitwise OR and XOR circuits

--- circuits/test/test_bitwise.py
+++ circuits/test/test_bitwise.py
@@ -32,3 +32,57 @@ class TestBitwiseAnd(unittest.TestCase):
 
                     result = int(''.join(map(str, c.evaluate())), 2)
                     self.assertEqual(n1 & n2, result)
+
+
+class TestBitwiseOr(unittest.TestCase):
+    def test_function(self):
+        # Test all possible operations up to 8 bits
+        for l in xrange(2, 9):
+
+            space = [[0, 1] for _ in xrange(0, l)]
+            it1 = itertools.product(*space)
+            it2 = itertools.product(*space)
+
+            for x in it1:
+                for y in it2:
+                    c = bitwise.bitwise_or_circuit(l)
+                    s = sources.DigitalArbitrary(x + y)
+
+                    n1 = int(''.join(map(str, x)), 2)
+                    n2 = int(''.join(map(str, y)), 2)
+
+                    source_c = circuit.Circuit('src', 0, 2 * l)
+                    source_c.add_output_component(s, {i: i for i in
+                                                      xrange(0, 2 * l)})
+                    circuit.connect_circuits(source_c, c,
+                                             {i: i for i in xrange(0, 2 * l)})
+
+                    result = int(''.join(map(str, c.evaluate())), 2)
+                    self.assertEqual(n1 | n2, result)
+
+
+class TestBitwiseXor(unittest.TestCase):
+    def test_function(self):
+        # Test all possible operations up to 8 bits
+        for l in xrange(2, 9):
+
+            space = [[0, 1] for _ in xrange(0, l)]
+            it1 = itertools.product(*space)
+            it2 = itertools.product(*space)
+
+            for x in it1:
+                for y in it2:
+                    c = bitwise.bitwise_xor_circuit(l)
+                    s = sources.DigitalArbitrary(x + y)
+
+                    n1 = int(''.join(map(str, x)), 2)
+                    n2 = int(''.join(map(str, y)), 2)
+
+                    source_c = circuit.Circuit('src', 0, 2 * l)
+                    source_c.add_output_component(s, {i: i for i in
+                                                      xrange(0, 2 * l)})
+                    circuit.connect_circuits(source_c, c,
+                                             {i: i for i in xrange(0, 2 * l)})
+
+                    result = int(''.join(map(str, c.evaluate())), 2)
+                    self.assertEqual(n1 ^ n2, result)
