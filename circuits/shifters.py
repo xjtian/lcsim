7dc32644391140e44bb8d800e7dd15ba56462777
Jacky Tian <xjtian94@gmail.com>
2013-05-03 17:52:01 -0400

    Right rotate modifier and doctest. Tweaked doctest for left_rotate to be a little more informative.

--- circuits/shifters.py
+++ circuits/shifters.py
@@ -15,19 +15,54 @@ def left_rotate(circuit, shift):
             Number of bits to rotate by.
 
     Returns:
-        The modified circuit (same object as the one passed in the parameter).
+        The modified circuit. The operation is done in place so this will be
+        the same reference as the parameter.
 
     Example usage:
         >>> from components import sources
         >>> from circuits import circuit
-        >>> s = sources.DigitalArbitrary([0, 1, 1, 0])
-        >>> c = circuit.Circuit('shift', 0, 4)
-        >>> c.add_output_component(s, {i: i for i in xrange(0, 4)})
+        >>> s = sources.DigitalArbitrary([0, 1, 1, 0, 1, 1])
+        >>> c = circuit.Circuit('shift', 0, 6)
+        >>> c.add_output_component(s, {i: i for i in xrange(0, 6)})
         >>> left_rotate(c, 2).evaluate()
-        [1, 0, 0, 1]
+        [1, 0, 1, 1, 0, 1]
     """
     output_size = len(circuit._outputs)
     shift %= output_size
 
     circuit._outputs = circuit._outputs[shift:] + circuit._outputs[:shift]
+    return circuit
+
+
+def right_rotate(circuit, shift):
+    """
+    Rotate (circular shift) the output bits of a given circuit to the right
+    by 'shift' bits in place. If the number of bits to shift exceeds the
+    output size of the circuit, the shift will be modded by the size of the
+    output space.
+
+    Parameters:
+        circuit:
+            The circuit to apply the right-rotate operation on.
+        shift:
+            Number of bits to rotate by.
+
+    Returns:
+        The modified circuit. The operation is done in place so this will be
+        the same reference as the parameter.
+
+    Example usage:
+        >>> from components import sources
+        >>> from circuits import circuit
+        >>> s = sources.DigitalArbitrary([0, 1, 1, 0, 1, 1])
+        >>> c = circuit.Circuit('shift', 0, 6)
+        >>> c.add_output_component(s, {i: i for i in xrange(0, 6)})
+        >>> right_rotate(c, 2).evaluate()
+        [1, 1, 0, 1, 1, 0]
+    """
+    output_size = len(circuit._outputs)
+    shift %= output_size
+
+    i = output_size - shift
+    circuit._outputs = circuit._outputs[i:] + circuit._outputs[:i]
     return circuit
