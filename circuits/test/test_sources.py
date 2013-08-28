585f3715600cc19e68e80a9e5578a7ce60edba8a
Jacky Tian <xjtian94@gmail.com>
2013-08-27 22:37:34 -0400

    Passing unit tests for int source circuit.

--- circuits/test/test_sources.py
+++ circuits/test/test_sources.py
@@ -11,3 +11,39 @@ class TestDigitalSource(unittest.TestCase):
         self.assertEqual('dSrc', c.name)
         self.assertEqual([0, 1, 1, 0, 0], c.evaluate())
         self.assertEqual(0, len(c._inputs))
+
+
+class TestDigitalSourceInt(unittest.TestCase):
+    def test_function(self):
+        c = sources.digital_source_int_circuit(0, 1)
+
+        self.assertEqual('dSrc', c.name)
+        self.assertEqual(0, len(c._inputs))
+
+    def test_no_padding(self):
+        c = sources.digital_source_int_circuit(31, 5)
+        self.assertEqual([1, 1, 1, 1, 1], c.evaluate())
+
+        c = sources.digital_source_int_circuit(63, 6)
+        self.assertEqual([1, 1, 1, 1, 1, 1], c.evaluate())
+
+        c = sources.digital_source_int_circuit(53, 6)
+        self.assertEqual([1, 1, 0, 1, 0, 1], c.evaluate())
+
+        c = sources.digital_source_int_circuit(1, 1)
+        self.assertEqual([1], c.evaluate())
+
+    def test_with_padding(self):
+        c = sources.digital_source_int_circuit(128, 16)
+        self.assertEqual([0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], c.evaluate())
+
+        c = sources.digital_source_int_circuit(77, 8)
+        self.assertEqual([0, 1, 0, 0, 1, 1, 0, 1], c.evaluate())
+
+    def test_hex(self):
+        c = sources.digital_source_int_circuit(0xDEAD, 16)
+        self.assertEqual([1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1], c.evaluate())
+
+        c = sources.digital_source_int_circuit(0xBEEF, 32)
+        self.assertEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                          1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1], c.evaluate())
