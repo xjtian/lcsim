b7b449a5f15f16675f5adf81b98edc47618137a5
Jacky Tian <xjtian94@gmail.com>
2013-08-27 22:27:18 -0400

    Digital number source circuit and passing doctest.

--- circuits/sources.py
+++ circuits/sources.py
@@ -1,5 +1,7 @@
 __author__ = 'Jacky'
 
+from math import log
+
 from components import sources
 from circuits import circuit
 
@@ -38,3 +40,50 @@ def digital_source_circuit(output):
         result.add_output_component(src, {i: 0})
 
     return result
+
+
+def digital_source_int_circuit(number, bits):
+    """
+    Create a digital source circuit that will always output the sequence
+    and number of bits that correspond to the given positive integer and
+    bit count. The circuit is composed entirely of DigitalOne and
+    DigitalZero components instead of DigitalArbitrary.
+
+    Parameters:
+        number:
+            The positive integer for the output sequence of bits.
+        bits:
+            How many bits should be used to represent the number.
+
+    Returns:
+        The resulting source circuit.
+
+    Raises:
+        ValueError if 'number' is negative, or the bit count is too low for
+        the given number.
+
+    Example usage:
+        >>> c = digital_source_int_circuit(78, 8)
+        >>> c.evaluate()
+        [0, 1, 0, 0, 1, 1, 1, 0]
+    """
+    if number < 0:
+        raise ValueError('Digital source output number must be unsigned.')
+
+    if number > (1 << bits) - 1:
+        raise ValueError('Digital source output number is too large to be '
+                         'represented by the given bit count.')
+
+    result = circuit.Circuit('dSrc', 0, bits)
+
+    o_bits = map(int, bin(number)[2:])
+    # Pad left with zeroes to get to bit count
+    zeroes = [0 for _ in xrange(0, bits - len(o_bits))]
+    zeroes.extend(o_bits)
+
+    comps = [sources.DigitalOne() if x else sources.DigitalZero() for x in zeroes]
+    for i, src in enumerate(comps):
+        result.add_output_component(src, {i: 0})
+
+    return result
+
