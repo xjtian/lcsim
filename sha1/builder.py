5cce9c00d11f361946f84d2d452c4e107cc8803b
Jacky Tian <xjtian94@gmail.com>
2013-09-10 21:46:20 -0400

    f calculation for first quarter of words in main loop, and passing test case.

--- sha1/builder.py
+++ sha1/builder.py
@@ -1,38 +1,53 @@
 __author__ = 'jacky'
 
 from circuits.sources import digital_source_int_circuit
+from circuits import bitwise, circuit
+
+h0 = digital_source_int_circuit(0x67452301, 32)
 
 
-def h0():
-    """
-    Returns a digital source circuit that evaluates to the h0 constant.
-    """
-    return digital_source_int_circuit(0x67452301, 32)
+h1 = digital_source_int_circuit(0xEFCDAB89, 32)
 
 
-def h1():
-    """
-    Returns a digital source circuit that evaluates to the h1 constant.
-    """
-    return digital_source_int_circuit(0xEFCDAB89, 32)
+h2 = digital_source_int_circuit(0x98BADCFE, 32)
 
 
-def h2():
-    """
-    Returns a digital source circuit that evaluates to the h2 constant.
-    """
-    return digital_source_int_circuit(0x98BADCFE, 32)
+h3 = digital_source_int_circuit(0x10325476, 32)
 
 
-def h3():
-    """
-    Returns a digital source circuit that evaluates to the h3 constant.
-    """
-    return digital_source_int_circuit(0x10325476, 32)
+h4 = digital_source_int_circuit(0xC3D2E1F0, 32)
 
 
-def h4():
+def f_first_quarter():
     """
-    Returns a digital source circuit that evaluates to the h4 constant.
+    Returns the circuit that evaluates (h1 and h2) or ((not h1) and h3).
+    In the pseudocode, this is f = (b and c) or ((not b) and d) for
+    0 <= i <= 19.
     """
-    return digital_source_int_circuit(0xC3D2E1F0, 32)
+    # (h1 and h2)
+    c_and = bitwise.bitwise_and_circuit(32)
+    stacked_inputs = circuit.stack_circuits('h1-h2 stack', h1, h2)
+    circuit.connect_circuits(stacked_inputs, c_and, {i: i for i in xrange(0, 64)})
+
+    h1_and_h2 = circuit.merge_circuits('h1 and h2', stacked_inputs, c_and)
+
+    # (not h1)
+    c_not = bitwise.bitwise_not_circuit(32)
+    circuit.connect_circuits(h1, c_not, {i: i for i in xrange(0, 32)})
+
+    not_h1 = circuit.merge_circuits('not h1', h1, c_not)
+
+    # ((not h1) and h3)
+    c_and = bitwise.bitwise_and_circuit(32)
+    stacked_inputs = circuit.stack_circuits('not h1-h3 stack', not_h1, h3)
+    circuit.connect_circuits(stacked_inputs, c_and, {i: i for i in xrange(0, 64)})
+
+    not_h1_and_h3 = circuit.merge_circuits('(not h1) and h3', stacked_inputs, c_and)
+
+    # (h1 and h2) or ((not h1) and h3)
+    c_or = bitwise.bitwise_or_circuit(32)
+    stacked_inputs = circuit.stack_circuits('final stack', h1_and_h2, not_h1_and_h3)
+    circuit.connect_circuits(stacked_inputs, c_or, {i: i for i in xrange(0, 64)})
+
+    result = circuit.merge_circuits('first quarter main loop', stacked_inputs, c_or)
+    return result
--- sha1/test/test_builder.py
+++ sha1/test/test_builder.py
@@ -5,37 +5,41 @@ from sha1 import builder
 
 
 class TestConstants(unittest.TestCase):
-    def test_h0(self):
-        result = builder.h0()
+    def setUp(self):
+        self.h0 = 0x67452301
+        self.h1 = 0xEFCDAB89
+        self.h2 = 0x98BADCFE
+        self.h3 = 0x10325476
+        self.h4 = 0xC3D2E1F0
 
+    def test_h0(self):
         self.assertEqual([0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1,
                           0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
-                         result.evaluate())
+                         builder.h0.evaluate())
 
     def test_h1(self):
-        result = builder.h1()
-
         self.assertEqual([1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1,
                           1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1],
-                         result.evaluate())
+                         builder.h1.evaluate())
 
     def test_h2(self):
-        result = builder.h2()
-
         self.assertEqual([1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0,
                           1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0],
-                         result.evaluate())
+                         builder.h2.evaluate())
 
     def test_h3(self):
-        result = builder.h3()
-
         self.assertEqual([0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0,
                           0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0],
-                         result.evaluate())
+                         builder.h3.evaluate())
 
     def test_h4(self):
-        result = builder.h4()
-
         self.assertEqual([1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0,
                           1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0],
-                         result.evaluate())
+                         builder.h4.evaluate())
+
+    def test_f_first_quarter(self):
+        f = builder.f_first_quarter()
+        result = int(''.join(map(str, f.evaluate())), 2)
+
+        expected = (self.h1 & self.h2) | ((~ self.h1) & self.h3)
+        self.assertEqual(expected, result)
