524c629482402b967ae96e9e1d5348d5b83cee80
Jacky Tian <xjtian94@gmail.com>
2013-11-19 14:46:22 -0500

    Untested code for SHA-1 chunk operation

--- sha1/builder.py
+++ sha1/builder.py
@@ -1,53 +1,174 @@
+from itertools import izip
+
+from circuits.bitwise import bitwise_or_circuit, bitwise_and_circuit, bitwise_not_circuit, bitwise_xor_circuit
+from circuits.circuit import connect_circuits
+from circuits.sources import digital_source_int_circuit
+from circuits.adders import ripple_adder_no_carry
+from circuits.shifters import left_rotate
+
 __author__ = 'jacky'
 
-from circuits.sources import digital_source_int_circuit
-from circuits import bitwise, circuit
 
-h0 = digital_source_int_circuit(0x67452301, 32)
-
-
-h1 = digital_source_int_circuit(0xEFCDAB89, 32)
-
-
-h2 = digital_source_int_circuit(0x98BADCFE, 32)
-
-
-h3 = digital_source_int_circuit(0x10325476, 32)
-
-
-h4 = digital_source_int_circuit(0xC3D2E1F0, 32)
-
-
-def f_first_quarter():
+def block_operation(chunk, h0, h1, h2, h3, h4):
     """
-    Returns the circuit that evaluates (h1 and h2) or ((not h1) and h3).
-    In the pseudocode, this is f = (b and c) or ((not b) and d) for
-    0 <= i <= 19.
+    Returns (h0, h1, h2, h3, h4), the h-constants that result from running
+    the SHA-1 algorithm on one block.
     """
-    # (h1 and h2)
-    c_and = bitwise.bitwise_and_circuit(32)
-    stacked_inputs = circuit.stack_circuits('h1-h2 stack', h1, h2)
-    circuit.connect_circuits(stacked_inputs, c_and, {i: i for i in xrange(0, 64)})
 
-    h1_and_h2 = circuit.merge_circuits('h1 and h2', stacked_inputs, c_and)
+    a, b, c, d, e = h0, h1, h2, h3, h4
 
-    # (not h1)
-    c_not = bitwise.bitwise_not_circuit(32)
-    circuit.connect_circuits(h1, c_not, {i: i for i in xrange(0, 32)})
+    w = [None] * 80
+    # Each element is a tuple of a circuit and the range of bits for it
+    # That is a horrendous explanation
 
-    not_h1 = circuit.merge_circuits('not h1', h1, c_not)
+    for i in xrange(0, 16):
+        w[i] = (chunk, xrange(i * 32, (i + 1) * 32))
+    for i in xrange(16, 80):
+        # w[i] = (w[i-3] xor w[i-8] xor w[i-14] xor w[i-16]) leftrotate 1
+        xtemp = bitwise_xor_circuit(32)
 
-    # ((not h1) and h3)
-    c_and = bitwise.bitwise_and_circuit(32)
-    stacked_inputs = circuit.stack_circuits('not h1-h3 stack', not_h1, h3)
-    circuit.connect_circuits(stacked_inputs, c_and, {i: i for i in xrange(0, 64)})
+        connect_circuits(w[i - 3][0], xtemp, {
+            x: y for (x, y) in izip(w[i - 3][1], xrange(0, 32))
+        })
 
-    not_h1_and_h3 = circuit.merge_circuits('(not h1) and h3', stacked_inputs, c_and)
+        connect_circuits(w[i - 8][0], xtemp, {
+            x: y for (x, y) in izip(w[i - 8][1], xrange(32, 64))
+        })
 
-    # (h1 and h2) or ((not h1) and h3)
-    c_or = bitwise.bitwise_or_circuit(32)
-    stacked_inputs = circuit.stack_circuits('final stack', h1_and_h2, not_h1_and_h3)
-    circuit.connect_circuits(stacked_inputs, c_or, {i: i for i in xrange(0, 64)})
+        # result xor w[i - 14]
+        xtemp2 = bitwise_xor_circuit(32)
+        connect_circuits(xtemp, xtemp2, {x: x for x in xrange(0, 32)})
+        connect_circuits(w[i - 14][0], xtemp2, {
+            x: y for (x, y) in izip(w[i - 14][1], xrange(32, 64))
+        })
 
-    result = circuit.merge_circuits('first quarter main loop', stacked_inputs, c_or)
-    return result
+        # result xor w[i - 16]
+        xtemp = bitwise_xor_circuit(32)
+        connect_circuits(xtemp2, xtemp, {x: x for x in xrange(0, 32)})
+        connect_circuits(w[i - 16][0], xtemp, {
+            x: y for (x, y) in izip(w[i - 16][1], xrange(32, 64))
+        })
+
+        # leftrotate 1
+        word = left_rotate(xtemp, 1)
+        w[i] = (word, xrange(0, 32))
+
+    # Main loop here
+    for i in xrange(0, 80):
+        if 0 <= i <= 19:
+            # f = (b and c) or ((not b) and d)
+            b_and_c = bitwise_and_circuit(32)
+            b_and_c.add_input_component(b, {x: x for x in xrange(0, 32)})
+            b_and_c.add_input_component(c, {x: x for x in xrange(32, 64)})
+
+            not_b = bitwise_not_circuit(32)
+            not_b.add_input_component(b, {x: x for x in xrange(0, 32)})
+
+            not_b_and_d = bitwise_and_circuit(32)
+            not_b_and_d.add_input_component(not_b, {x: x for x in xrange(0, 32)})
+            not_b_and_d.add_input_component(d, {x: x for x in xrange(32, 64)})
+
+            f = bitwise_or_circuit(32)
+            f.add_input_component(b_and_c, {x: x for x in xrange(0, 32)})
+            f.add_input_component(not_b_and_d, {x: x for x in xrange(32, 64)})
+
+            k = digital_source_int_circuit(0x6ED9EBA1, 32)
+        elif 20 <= i <= 39:
+            # f = b xor c xor d
+            b_xor_c = bitwise_xor_circuit(32)
+            b_xor_c.add_input_component(b, {x: x for x in xrange(0, 32)})
+            b_xor_c.add_input_component(c, {x: x for x in xrange(32, 64)})
+
+            f = bitwise_xor_circuit(32)
+            f.add_input_component(b_xor_c, {x: x for x in xrange(0, 32)})
+            f.add_input_component(d, {x: x for x in xrange(32, 64)})
+
+            k = digital_source_int_circuit(0x6ED9EBA1, 32)
+        elif 40 <= i <= 59:
+            # f = (b and c) or (b and d) or (c and d)
+            b_and_c = bitwise_and_circuit(32)
+            b_and_c.add_input_component(b, {x: x for x in xrange(0, 32)})
+            b_and_c.add_input_component(c, {x: x for x in xrange(32, 64)})
+
+            b_and_d = bitwise_and_circuit(32)
+            b_and_d.add_input_component(b, {x: x for x in xrange(0, 32)})
+            b_and_d.add_input_component(d, {x: x for x in xrange(32, 64)})
+
+            c_and_d = bitwise_and_circuit(32)
+            c_and_d.add_input_component(c, {x: x for x in xrange(0, 32)})
+            c_and_d.add_input_component(d, {x: x for x in xrange(32, 64)})
+
+            bnc_or_bnd = bitwise_or_circuit(32)
+            bnc_or_bnd.add_input_component(b_and_c, {x: x for x in xrange(0, 32)})
+            bnc_or_bnd.add_input_component(b_and_d, {x: x for x in xrange(32, 64)})
+
+            f = bitwise_or_circuit(32)
+            f.add_input_component(bnc_or_bnd, {x: x for x in xrange(0, 32)})
+            f.add_input_component(c_and_d, {x: x for x in xrange(32, 64)})
+
+            k = digital_source_int_circuit(0x8F1BBCDC, 32)
+        elif 60 <= i <= 79:
+            # f = b xor c xor d
+            b_xor_c = bitwise_xor_circuit(32)
+            b_xor_c.add_input_component(b, {x: x for x in xrange(0, 32)})
+            b_xor_c.add_input_component(c, {x: x for x in xrange(32, 64)})
+
+            f = bitwise_xor_circuit(32)
+            f.add_input_component(b_xor_c, {x: x for x in xrange(0, 32)})
+            f.add_input_component(d, {x: x for x in xrange(32, 64)})
+
+            k = digital_source_int_circuit(0xCA62C1D6, 32)
+        else:
+            raise Exception("Invalid word index in main loop!")
+
+        # (a leftrotate 5) + f
+        temp = ripple_adder_no_carry(32)
+        connect_circuits(a, temp, {x: x - 27 for x in xrange(27, 32)})
+        connect_circuits(a, temp, {x: x for x in xrange(0, 27)})
+        connect_circuits(f, temp, {x: x for x in xrange(0, 32)})
+
+        # result + e
+        temp2 = ripple_adder_no_carry(32)
+        connect_circuits(temp, temp2, {x: x for x in xrange(0, 32)})
+        connect_circuits(e, temp2, {x: x + 32 for x in xrange(0, 32)})
+
+        # result + k
+        temp = ripple_adder_no_carry(32)
+        connect_circuits(temp2, temp, {x: x for x in xrange(0, 32)})
+        connect_circuits(k, temp, {x: x + 32 for x in xrange(0, 32)})
+
+        # result + w[i]
+        temp2 = ripple_adder_no_carry(32)
+        connect_circuits(temp, temp2, {x: x for x in xrange(0, 32)})
+        connect_circuits(w[i][0], temp2, {x: y for (x, y) in izip(w[i][1], xrange(32, 64))})
+
+        # temp = (a leftrotate 5) + f + e + k + w[i]
+        temp = temp2
+
+        e = d
+        d = c
+        c = left_rotate(b, 30)
+        b = a
+        a = temp
+
+    h0_add = ripple_adder_no_carry(32)
+    connect_circuits(h0, h0_add, {i: i for i in xrange(0, 32)})
+    connect_circuits(a, h0_add, {i: i + 32 for i in xrange(0, 32)})
+
+    h1_add = ripple_adder_no_carry(32)
+    connect_circuits(h1, h1_add, {i: i for i in xrange(0, 32)})
+    connect_circuits(b, h1_add, {i: i + 32 for i in xrange(0, 32)})
+
+    h2_add = ripple_adder_no_carry(32)
+    connect_circuits(h2, h2_add, {i: i for i in xrange(0, 32)})
+    connect_circuits(c, h2_add, {i: i + 32 for i in xrange(0, 32)})
+
+    h3_add = ripple_adder_no_carry(32)
+    connect_circuits(h3, h3_add, {i: i for i in xrange(0, 32)})
+    connect_circuits(d, h3_add, {i: i + 32 for i in xrange(0, 32)})
+
+    h4_add = ripple_adder_no_carry(32)
+    connect_circuits(h4, h4_add, {i: i for i in xrange(0, 32)})
+    connect_circuits(e, h4_add, {i: i + 32 for i in xrange(0, 32)})
+
+    return h0_add, h1_add, h2_add, h3_add, h4_add
