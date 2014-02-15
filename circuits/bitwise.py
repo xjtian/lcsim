7ebcdc407ee47bce5daf2400729d59674fe68936
Jacky Tian <xjtian94@gmail.com>
2014-02-15 17:33:13 -0500

    Refactor bitwise operation circuits and tests.

--- circuits/bitwise.py
+++ circuits/bitwise.py
@@ -18,17 +18,6 @@ def bitwise_and_circuit(bits):
 
     Returns:
         Bitwise AND circuit for 2 inputs of the specified size.
-
-    Example usage:
-        >>> from components import sources
-        >>> from circuits import circuit
-        >>> s = sources.DigitalArbitrary([0, 1, 1, 0, 0, 1, 0, 1])
-        >>> c = circuit.Circuit('in1', 0, 8)
-        >>> c.add_output_component(s, {i: i for i in xrange(0, 8)})
-        >>> and_c = bitwise_and_circuit(4)
-        >>> circuit.connect_circuits(c, and_c, {i: i for i in xrange(0, 8)})
-        >>> and_c.evaluate()
-        [0, 1, 0, 0]
     """
     result = circuit.Circuit('bAND', 2*bits, bits)
     comps = [gates.ANDGate() for _ in xrange(0, bits)]
@@ -37,7 +26,7 @@ def bitwise_and_circuit(bits):
         result.add_input_component(comps[i], {i: 0})
         result.add_input_component(comps[i], {i + bits: 1})
 
-        result.add_output_component(comps[i], {i: 0})
+        result.add_output_component(comps[i], i)
 
     return result
 
@@ -56,17 +45,6 @@ def bitwise_or_circuit(bits):
 
     Returns:
         Bitwise OR circuit for 2 inputs of the specified size.
-
-    Example usage:
-        >>> from components import sources
-        >>> from circuits import circuit
-        >>> s = sources.DigitalArbitrary([0, 1, 1, 0, 0, 1, 0, 1])
-        >>> c = circuit.Circuit('in1', 0, 8)
-        >>> c.add_output_component(s, {i: i for i in xrange(0, 8)})
-        >>> or_c = bitwise_or_circuit(4)
-        >>> circuit.connect_circuits(c, or_c, {i: i for i in xrange(0, 8)})
-        >>> or_c.evaluate()
-        [0, 1, 1, 1]
     """
     result = circuit.Circuit('bOR', 2*bits, bits)
     comps = [gates.ORGate() for _ in xrange(0, bits)]
@@ -75,7 +53,7 @@ def bitwise_or_circuit(bits):
         result.add_input_component(comps[i], {i: 0})
         result.add_input_component(comps[i], {i + bits: 1})
 
-        result.add_output_component(comps[i], {i: 0})
+        result.add_output_component(comps[i], i)
 
     return result
 
@@ -94,17 +72,6 @@ def bitwise_xor_circuit(bits):
 
     Returns:
         Bitwise XOR circuit for 2 inputs of the specified size.
-
-    Example usage:
-        >>> from components import sources
-        >>> from circuits import circuit
-        >>> s = sources.DigitalArbitrary([0, 1, 1, 0, 0, 1, 0, 1])
-        >>> c = circuit.Circuit('in1', 0, 8)
-        >>> c.add_output_component(s, {i: i for i in xrange(0, 8)})
-        >>> xor_c = bitwise_xor_circuit(4)
-        >>> circuit.connect_circuits(c, xor_c, {i: i for i in xrange(0, 8)})
-        >>> xor_c.evaluate()
-        [0, 0, 1, 1]
     """
     result = circuit.Circuit('bXOR', 2*bits, bits)
     comps = [gates.XORGate() for _ in xrange(0, bits)]
@@ -113,7 +80,7 @@ def bitwise_xor_circuit(bits):
         result.add_input_component(comps[i], {i: 0})
         result.add_input_component(comps[i], {i + bits: 1})
 
-        result.add_output_component(comps[i], {i: 0})
+        result.add_output_component(comps[i], i)
 
     return result
 
@@ -129,23 +96,12 @@ def bitwise_not_circuit(bits):
 
     Returns:
         Bitwise NOT circuit for specified input size.
-
-    Example usage:
-        >>> from components import sources
-        >>> from circuits import circuit
-        >>> s = sources.DigitalArbitrary([0, 1, 1, 0])
-        >>> c = circuit.Circuit('in1', 0, 4)
-        >>> c.add_output_component(s, {i: i for i in xrange(0, 4)})
-        >>> not_c = bitwise_not_circuit(4)
-        >>> circuit.connect_circuits(c, not_c, {i: i for i in xrange(0, 4)})
-        >>> not_c.evaluate()
-        [1, 0, 0, 1]
     """
     result = circuit.Circuit('bNOT', bits, bits)
     comps = [gates.NOTGate() for _ in xrange(0, bits)]
 
     for i in xrange(0, bits):
         result.add_input_component(comps[i], {i: 0})
-        result.add_output_component(comps[i], {i: 0})
+        result.add_output_component(comps[i], i)
 
     return result
