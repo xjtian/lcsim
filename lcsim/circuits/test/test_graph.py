260a69d955c40c2dc853240d08258d904d6fa98d
Jacky Tian <xjtian94@gmail.com>
2014-02-15 21:56:01 -0500

    320 as min-cut size is not correct...

--- lcsim/circuits/test/test_graph.py
+++ lcsim/circuits/test/test_graph.py
@@ -3,6 +3,8 @@ import sys
 import random
 import unittest
 
+import networkx as nx
+
 from lcsim.circuits.graph import to_graph
 from lcsim.circuits import sources
 from lcsim.sha1 import builder
@@ -15,10 +17,34 @@ class TestToGraph(unittest.TestCase):
         sys.setrecursionlimit(100000)
 
         message_circuit = sources.digital_source_int_circuit(random.getrandbits(512), 512)
-        builder.sha1(message_circuit)
+        _, h = builder.sha1(message_circuit)
 
         g = to_graph(message_circuit._outputs)
-        print len(g.nodes())
-        print len(g.edges())
 
-        print ComponentBase.count
+        # All gates/nodes that input hooks into
+        # a = set()
+        # for gate in message_circuit._outputs:
+        #     a |= set(g.neighbors(gate))
+        #     g.remove_node(gate)
+        #
+        # g.add_node('source')
+        # for gate in a:
+        #     g.add_edge('source', gate)
+        #
+        # g.add_node('sink')
+        # for gate in h._outputs:
+        #     g.add_edge(gate, 'sink')
+
+        g.add_node('source')
+        for gate in message_circuit._outputs:
+            g.add_edge('source', gate)
+
+        g.add_node('sink')
+        for gate in h._outputs:
+            g.add_edge(gate, 'sink')
+
+        print 'Number of nodes in circuit graph: %d' % len(g.nodes())
+        print 'Number of edges in circuit graph: %d' % len(g.edges())
+        print 'Total number of instantiated components: %d' % ComponentBase.count
+
+        print 'Min-cut size: %d' % nx.max_flow(g, 'source', 'sink')
